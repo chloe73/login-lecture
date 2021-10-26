f841b31913b682d189a9b225e079c509108e5294
chloe73 <esom735@gmail.com>
2021-10-27 01:54:50 +0900

    User 모델 구현

--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -2,6 +2,7 @@
 
 const { use } = require(".");
 const UserStage = require("../../models/UserStorage");
+const User = require("../../models/User");
 
 const output = {
     home: (req, res) =>{
@@ -15,23 +16,10 @@ const output = {
 
 const process = {
     login: (req,res) => {
-        const id = req.body.id,
-            psword = req.body.psword;
-    
-    const users = UserStage.getUsers("id", "psword");
-
-    const response = {};
-    if (users.id.includes(id)) {
-        const idx = users.id.indexOf(id);
-        if (users.psword[idx] === psword) {
-            response.success = true;
-            return res.json(response);
-        }
-    }
-
-    response.success = false;
-    response.msg = "로그인에 실패하셨습니다.";
-    return res.json(response);
+        const user = new User(req.body);
+        const response = user.login();
+        console.log(response);
+        return res.json(response);
     },
 };
 
